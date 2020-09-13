47ada02ecad6796b867d60cc7e757aa89302ab06
Charles Alves <charles.siqueira@gmail.com>
2020-09-13 20:15:34 -0300

    Corrigindo teste do ExportFileTasklet

--- src/test/java/br/com/charlesalves/batchcompilation/tasklets/ExportFileTaskletTest.java
+++ src/test/java/br/com/charlesalves/batchcompilation/tasklets/ExportFileTaskletTest.java
@@ -8,17 +8,12 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.apache.tomcat.util.http.fileupload.FileUtils;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -27,11 +22,8 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
-import br.com.charlesalves.batchcompilation.util.IOUtils;
-
 @SpringBootTest
 @ActiveProfiles("test")
-@TestInstance(Lifecycle.PER_CLASS)
 @Sql(scripts = "/insert-data.sql")
 @Sql(scripts = "/clean-up-data.sql", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
 public class ExportFileTaskletTest {
@@ -50,31 +42,19 @@ public class ExportFileTaskletTest {
 
 	private File outputFile;
 
-	@BeforeAll
-	public void setupAll() throws URISyntaxException, IOException {
-		Path source = Paths.get(ClassLoader.getSystemResource("sales.dat").toURI());
-		Path target = Paths.get(testDir + "/in/sales.dat");
-
-		IOUtils.copy(source, target);
-	}
-
-	@AfterAll
-	public void terdownAll() throws IOException {
-		File testDirFile = new File(testDir);
-
-		FileUtils.deleteDirectory(testDirFile);
-	}
-
 	@BeforeEach
-	public void setup() {
+	public void setup() throws URISyntaxException, IOException {
 		outputFile = new File(outputPath);
 	}
 
 	@AfterEach
-	public void teardown() {
+	public void teardown() throws IOException {
 		if (outputFile.exists()) {
 			outputFile.delete();
 		}
+
+		File testDirFile = new File(testDir);
+		FileUtils.deleteDirectory(testDirFile);
 	}
 
 	@Test
@@ -83,8 +63,8 @@ public class ExportFileTaskletTest {
 
 		assertThat(status, is(equalTo(RepeatStatus.FINISHED)));
 		assertThat(outputFile.exists(), is(true));
-		String fileData = getFileData();
 
+		String fileData = getFileData();
 		assertThat(fileData, is(equalTo(getTestData())));
 	}
 
